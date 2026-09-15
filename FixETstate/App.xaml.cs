19e416420d01220bc08456308b216e679c14af0f
Innocent777 <innoradam@gmail.com>
2026-09-16 01:12:05 +0200

    Add project files.

---
+++ FixETstate/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FixETstate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ FixETstate/ResetRequestWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FixETstate
+{
+    public partial class ResetRequestWindow : Window
+    {
+        public ResetRequestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Reset approved.\n\nThe next version will perform the approved safe reset and restart the computer.",
+                "Reset Approved",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+
+            DialogResult = true;
+        }
+
+        private void DeclineButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "The reset request was declined. No changes were made.",
+                "Request Declined",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+
+            DialogResult = false;
+        }
+    }
+}
